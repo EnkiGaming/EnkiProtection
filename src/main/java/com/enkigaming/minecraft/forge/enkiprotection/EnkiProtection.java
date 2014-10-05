05cac39e6f1f4ac2715d5cd0aed3f2d21aea2c6b
HaniiPuppy <hanii.puppy@googlemail.com>
2014-10-05 16:34:21 +0100

    Updated the mod class.
    Updated the mod class, now server side. Added getInstance

--- src/main/java/com/enkigaming/minecraft/forge/enkiprotection/EnkiProtection.java
+++ src/main/java/com/enkigaming/minecraft/forge/enkiprotection/EnkiProtection.java
@@ -3,16 +3,24 @@ package com.enkigaming.minecraft.forge.enkiprotection;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.world.ChunkPosition;
+import net.minecraft.world.chunk.Chunk;
 
-@Mod(modid = EnkiProtection.MODID, version = EnkiProtection.VERSION)
+@Mod(modid = EnkiProtection.MODID, name = EnkiProtection.NAME, version = EnkiProtection.VERSION, acceptableRemoteVersions = "*")
 public class EnkiProtection
 {
+    public static final String NAME = "EnkiProtection";
     public static final String MODID = "EnkiProtection";
     public static final String VERSION = "1.0";
+    
+    static EnkiProtection instance;
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        
+        EnkiProtection.instance = this;
     }
+    
+    public static EnkiProtection getInstance()
+    { return instance; }
 }
