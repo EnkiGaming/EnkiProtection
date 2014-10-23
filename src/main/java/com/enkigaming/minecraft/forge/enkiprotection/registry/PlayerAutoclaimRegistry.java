97fc1b7c7679151867aa22fd4866f8ba31a22aa7
HaniiPuppy <hanii.puppy@googlemail.com>
2014-10-23 15:47:23 +0100

    Implemented PlayerAutoclaimRegistry

--- src/main/java/com/enkigaming/minecraft/forge/enkiprotection/registry/PlayerAutoclaimRegistry.java
+++ src/main/java/com/enkigaming/minecraft/forge/enkiprotection/registry/PlayerAutoclaimRegistry.java
@@ -33,17 +33,34 @@ public class PlayerAutoclaimRegistry
     final Map<UUID, AutoclaimState> claimStates = new HashMap<UUID, AutoclaimState>();
     
     public AutoclaimState getState(UUID playerId)
-    {  }
+    {
+        synchronized(claimStates)
+        { return claimStates.get(playerId); }
+    }
+    
+    public AutoclaimState setState(UUID playerId, AutoclaimState state)
+    {
+        synchronized(claimStates)
+        {
+            if(state == null || state.getSetting() == null)
+                return claimStates.remove(playerId);
+            
+            return claimStates.put(playerId, state);
+        }
+    }
     
     public AutoclaimState setState(UUID playerId, UUID claimId, AutoclaimSetting setting)
-    {  }
+    { return setState(playerId, new AutoclaimState(claimId, setting)); }
     
     public AutoclaimState setClaiming(UUID playerId, UUID claimId)
-    {  }
+    { return setState(playerId, claimId, AutoclaimSetting.claiming); }
     
     public AutoclaimState setUnclaiming(UUID playerId, UUID claimId)
-    {  }
+    { return setState(playerId, claimId, AutoclaimSetting.unclaiming); }
     
     public AutoclaimState setDoingNothing(UUID playerId)
-    {  }
+    {
+        synchronized(claimStates)
+        { return claimStates.remove(playerId); }
+    }
 }
